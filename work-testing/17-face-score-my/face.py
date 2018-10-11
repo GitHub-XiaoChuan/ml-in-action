fc48c09f8c0c52f2968bd6dadf4605c53f0f615a
xinghalo <xinghalo@163.com>
2018-10-11 14:26:12 +0800

    人脸识别与颜值预测 性别预测

--- work-testing/17-face-score-my/face.py
+++ work-testing/17-face-score-my/face.py
@@ -3,7 +3,8 @@ import face_recognition
 from keras.models import load_model
 import numpy as np
 
-model = load_model('faceRank.h5')
+score_model = load_model('faceRank.h5')
+sex_model = load_model('faceSex.h5')
 
 def recognition():
     camera = cv2.VideoCapture(0)
@@ -16,17 +17,19 @@ def recognition():
 
         for (top, right, bottom, left) in face_locations:
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-            x = left
-            y = top
-            w = right - left
-            h = bottom - top
-            #image = frame[]
 
-            image = cv2.cvtColor(cv2.resize(gray[y:y + h, x:x + w], (128, 128)), cv2.COLOR_GRAY2BGR)
+            image = cv2.cvtColor(cv2.resize(gray[top:bottom, left:right], (128, 128)), cv2.COLOR_GRAY2BGR)
             image = np.array(image).reshape(1, 128, 128, 3)
-            p = model.predict_classes([image])
 
-            cv2.putText(frame, 'score:'+str(p[0]), (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            score = score_model.predict_classes(image)
+            sex = sex_model.predict_classes(image)
+
+            if sex == 0:
+                sex = 'male'
+            else:
+                sex = 'female'
+
+            cv2.putText(frame, str(score[0])+' '+sex, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         cv2.imshow("video", frame)
 
